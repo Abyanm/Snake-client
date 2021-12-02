8b680fe554ca1a5c82239ca663a9d853c797ccdc
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-12-02 21:44:12 +0000

    Stdin setup

--- play.js
+++ play.js
@@ -23,4 +23,30 @@ const connect = function () {
 console.log("Connecting..");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (direction) {
+  if (direction === "w"){
+    connect.write("move up");
+    if (direction === "a") {
+      connect.write("move left");
+      if (direction === "d"){
+        connect.write("move right");
+        if(direction === "x");
+        connect.write("move down");
+        if(direction === "q"){
+          connect.write("Well done")
+        }
+      }
+return connect
+    }
+  }
+};
+
 module.exports = {connect};
